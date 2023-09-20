d1392884503559aaa2c59400715ee8eb05fdce78
Sperry <1080333@macomb.edu>
2023-09-20 15:10:51 -0400

    added sin.py

---
+++ sin.py
@@ -0,0 +1,75 @@
+#may mercy be put on your soul if you decide to endure reading this code, I think its funny but my sense of humor is awful. 
+word = str()
+word1 = str()
+
+word = "Hewwo Mr.tax-man :3"
+word1 = "Testing"
+word2 = "Hi I'm here to make you pay taxes"
+
+for mary in word:
+    print(mary)
+
+a = int()
+a = len("this is a catastrpohe like a silent orchestra for an audiance of blind people")
+print(a)
+
+need = "Hewwo"
+
+if need in word:
+    print("why can't you just be normal?")
+else:
+    print("Oh good at least your sane")
+    #For every line of code I write my sanity dwindles by 1 point :(
+
+##if "taxes" in word2:
+##    print("IM NOT PAYING MY DANG TAXES")
+##    #I anit payin my dang taxes
+##elif "pay" in word2:
+##    print("I don't know or care what it is I'm not paying >:(")
+##    #could be a scam anyways
+##else:
+##    print("Thank you for not trying to make me pay for anythin")
+
+if "taxes" in word2:
+    print("IM NOT PAYING MY DANG TAXES")
+    #I anit payin my dang taxes
+if "pay" in word2:
+    print("I don't know or care what it is I'm not paying >:(")
+    #could be a scam anyways
+else:
+    print("Thank you for not trying to make me pay for anythin")
+#this is wrong and wack but at least it dosen't ruin the joke
+
+print(word[1])
+word2 = word +" "+ word1
+print(word + word2)
+#lord this entire script is insanity, I wonder if it would count as a virus if i got it on someones computer without their will.
+#wait no this dosn't technically do any really damge to the pc, only pyschological damage so it's not a virus.
+
+VOWELS = str()
+VOWELS = "aeiou"
+TAXES = str()
+TAXES = "tax"
+i = int()
+i = 0
+
+while i <= 15:
+    i = i + 1
+    for v in VOWELS:
+        print(v)
+        for letter in word:
+            print(letter)
+            if v == letter:
+                print("is in : " + v)
+            else:
+                print("aw bollocks its not in there")
+
+    for t in TAXES:
+        print(t)
+        for letter in word:
+            print(letter)
+            if t == letter:
+                print("IM NOT PAYING TAXES")
+            else:
+                print("No taxes here")
+
