23f04aee9fa0dc48ea8485980b48d8abc2974a9e
IchoDev <jms01192006@gmail.com>
2023-10-30 14:03:01 -0400

    Added some list softwere

---
+++ EgoHell.py
@@ -0,0 +1,54 @@
+ego = ["test",["Solem Lament" ,1 , 0.5 , 1 ,2], ["Mimicry", 1, 3 , 2 ,1], ["Justitia", 2 , 2 , 2 , 2]]
+
+##print(ego)
+###expecting entire list
+##print(ego[1])
+###expecting all of mimicry
+##print(ego[1][1])
+###expecting only mimicry damage numbers
+##print(ego[1][1][1])
+###expecting second damage number only
+
+def egoF(e):
+    l = e
+    ui = input("What EGO are you trying to find? : ")
+    if ui in ego:
+        print(l.index(ui))
+
+def egoM(e):
+    l = e
+    c = 0
+    new = []
+    ui = input("Enter the name of your EGO : ")
+    new.append(ui)
+    while c <= 3:
+        ui = input("Enter a damge number : ")
+        new.append(ui)
+        c += 1
+    print(new)
+    l.append(new)
+    return l
+
+def egoE(e):
+    l = e
+    i1 = int(input("Enter the first index to find your EGO : "))
+    i2 = int(input("Enter the second index to find your EGO : "))
+    v = input("Enter what the value should be set to : ")
+    if i2 != 0:
+        int(v)
+    l[i1][i2] = v
+    return l
+def main(e):
+    ui = int(input("Are you 1) Entering 2) Looking For 3) Editing an EGO? : "))
+    if ui == 1:
+        e = egoM(e)
+        print(e)
+    elif ui == 2:
+        egoF(e)
+    elif ui == 3:
+        e = egoE(e)
+    else:
+        print("That Wasn't a choice")
+    print(e)
+
+main(ego)
--- Listem.py
+++ Listem.py
@@ -1,7 +1,7 @@
 inventory = []
 
-if "Da Capo" in inventory:
-    print("The beauty of a silent symphony!")
+if "Sound of a Star" in inventory:
+    ("LET US MEET AGAIN IN THE STARS!")
 else:
     print("Go get some E.G.O's nerd")
     
@@ -17,7 +17,31 @@ print(inventory[3])
 
 inventory.append("Sound of a Star")
 
-print(inventory[4])
+print(inventory)
 
 if "Sound of a Star" in inventory:
     print("LET US MEET AGAIN IN THE STARS!")
+
+inventory.append("23")
+inventory.sort()
+print(inventory)
+inventory.reverse()
+print(inventory)
+print(inventory.count("Mimicry"))
+print(inventory.index("Sound of a Star"))
+inventory.insert(3, "[CENSORD]")
+print(inventory.index("[CENSORD]"))
+print(inventory)
+inventory.remove("[CENSORD]")
+print(inventory)
+
+inner_ego = []
+inner_ego = ["Fifth Force" , "Repressed EGO"]
+inventory += inner_ego
+print(inventory)
+
+inventory.sort()
+inventory.reverse()
+print(inventory)
+inventory[3:6] = ["Corruptive E.G.O."]
+print(inventory)
